60a6eeca530d825ef45c24e3fec369d5ad46ccd0
rileydevine <824055@mystma.org>
2021-09-29 09:36:22 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define is even
+def isEven(n):
+    #if n divided by 2 equals 0
+    if (n%2)==0:
+
+        #return True
+        return True
+    
+    #if otherwise
+    else:
+        return False
+
+#print is even
+print(isEven(6))
